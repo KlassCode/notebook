e598b09a7d14ef4655cb4ae6420a80fe8304e0a7
KlassCode <=klassenedmond7@gmail.com>
2021-10-22 14:55:52 -0400

    Initialize Project

---
+++ app/Http/Controllers/ContactController.php
@@ -0,0 +1,102 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Contact;
+use Illuminate\Http\Request;
+
+class ContactController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //list all contacts for user connected
+        $user = auth()->user();
+        $contacts = Contact::where('user_id', '=', $user->id)->latest()->get();
+        
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //Add new Contact
+        $contact = new Contact([
+            'name'=>$request->input('name'),
+            'phone'=>$request->input('phone'),
+            'email'=>$request->input('email')
+        ]);
+
+        $contact->save();
+
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Contact  $contact
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //show the contact
+        $contact = Contact::findOrFail($id);
+
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Contact  $contact
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Contact $contact)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Contact  $contact
+     * @return \Illuminate\Http\Response
+     */
+    public function update($id,Request $request)
+    {
+        //update the contact
+        $contact = Contact::find($id);
+        $contact->update($request->all());
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Contact  $contact
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //destroy the contact
+        $contact = Contact::find($id);
+        $contact->delete();
+    }
+}
---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class UserController extends Controller
+{
+    /**
+     * show form to user to register or login
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        $user = Auth::user();
+        return response()->json($user,200);
+    }
+
+    /**
+     * function that help user login on notebook app
+     *
+     * @return json response
+     */
+
+    public function login(Request $request){
+        if(Auth::attempt(['email'=>$request->email,'password'=>$request->password],true)){
+            return response()->json(Auth::user(),200);
+        }else{
+            return response()->json(['error'=>'Erreur de Connexion email/mot de passe incorrect'],401);
+        }
+    }
+
+    public function register(Request $request){
+
+        $user = User::where('email',$request->email)->first();
+
+        if(isset($user->id)){
+            return response()->json(['error'=>'email deja utilisé'],401);   
+        }else{
+            
+            $user = new User();
+
+            $user->email = $request->email;
+            $suer->password = bcrypt($request->password);
+            $user->save();
+            
+            Auth::login($user);
+
+            return response()->json($user,200);
+
+        }
+
+    }
+
+}
